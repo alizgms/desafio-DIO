1a8d4a0ea1fc1effcee56b30b4b87c5095860267
Anna Alice Guimarães <justadaliz@gmail.com>
2021-01-08 00:40:55 -0300

    first commit

---
+++ UltrapassandoV.cs
@@ -0,0 +1,29 @@
+using System;
+
+class UltrapassandoV {
+    static void Main () {
+
+        var R = Int32.Parse(Console.ReadLine());
+        var V = 0;
+        var contador = 1;
+
+        do {
+
+         V = Int32.Parse(Console.ReadLine());
+
+        } while (V <= R);
+
+        int i = R;
+
+        while (i <= V) {
+
+         contador++;
+
+        R += 1;
+        i += R;
+        }
+
+    Console.WriteLine(contador);
+
+    }
+}
