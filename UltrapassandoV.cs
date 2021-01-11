4139428cad64783b5ab02a583edab18388ca6e3a
Anna Alice Guimarães <justadaliz@gmail.com>
2021-01-11 01:45:12 -0300

    Update UltrapassandoV

--- UltrapassandoV.cs
+++ UltrapassandoV.cs
@@ -3,27 +3,28 @@ using System;
 class UltrapassandoV {
     static void Main () {
 
-        var R = Int32.Parse(Console.ReadLine());
-        var V = 0;
-        var contador = 1;
+        var R = Int32.Parse(Console.ReadLine()); // recebe o valor de R
+        var V = 0; // inicializa o valor de V
+        var contador = 1; // inicializa o contador
 
         do {
 
-         V = Int32.Parse(Console.ReadLine());
+            V = Int32.Parse(Console.ReadLine()); // recebe o valor de V, até que o valor de V seja menor ou igual a R
 
         } while (V <= R);
 
-        int i = R;
+        int i = R; // "i" é usado como auxiliar
 
-        while (i <= V) {
+        while (i <= V) { 
 
-         contador++;
+            contador++; //enquanto o valor i for menor igual a V, o contador incrementa +1 
 
-        R += 1;
-        i += R;
+            R += 1; // R recebe o valor de R + 1
+            i += R; // i recebe o valor de i + R 
+
         }
 
-    Console.WriteLine(contador);
+        Console.WriteLine(contador); // mostra a quantidade de vezes que foi necessário para fazer a ultrapassagem
 
     }
 }
